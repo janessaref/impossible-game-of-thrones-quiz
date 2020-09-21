e6c39465b608c35983e5a3e2eec5cce27b6502eb
Game Over <janessarefong@gmail.com>
2020-09-21 15:53:11 -0700

    psuedocode in javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+GoT Quiz
+1. start the game
+    - event listener
+    - start the timer
+    - score starts at 0
+2. questions
+    - event listener 
+    - correct answers: add to score
+    - incorrect answers: subtract time
+    - change to next question
+3. high score 
+    - if timer is = 0, stop game and show final score and input initials
+    - if player finishes game, show final score and input initials
+    - local storage high score values
+    - separate link to view high scores
+*/
